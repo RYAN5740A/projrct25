f58223b9990e91cb37c500ab6b2118b2a2d11bd2
RYAN5740A <81171121+RYAN5740A@users.noreply.github.com>
2021-06-16 19:52:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,6 +38,6 @@ function draw() {
 }
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paperObject,paperObject.body.position,{x:130,y:-145});
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
 	}
 }
